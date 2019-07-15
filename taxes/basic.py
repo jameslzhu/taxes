084b4044cb063440350f27c7ffa4104fe0912569
James Zhu <jameszhu@berkeley.edu>
2019-07-14 18:31:33 -0700

    Attrs, cleanup with black

--- taxes/basic.py
+++ taxes/basic.py
@@ -2,11 +2,11 @@ import functools
 import os
 import re
 import timeit
+import typing
 import decimal
 from decimal import Decimal
 
-from typing import List
-
+import attr
 from bs4 import BeautifulSoup, Tag
 import requests
 
@@ -17,28 +17,22 @@ p17_url = "https://www.irs.gov/publications/p17"
 p17_file = "p17.html"
 
 
+@attr.s(auto_attribs=True)
 class Bracket:
-    def __init__(self, lower: Decimal, upper: Decimal, rate: Decimal):
-        self.lower = lower
-        self.upper = upper
-        self.rate = rate
-    
+    lower: Decimal
+    upper: Decimal
+    rate: Decimal
+
     def __str__(self):
         return "[{} - {} @ {}%]".format(self.lower, self.upper, self.rate)
-    
-    def __repr__(self):
-        return "Bracket({}, {}, {})".format(self.lower, self.upper, self.rate)
 
 
+@attr.s(auto_attribs=True)
 class Schedule:
-    def __init__(self, brackets: List[Bracket]):
-        self.brackets = brackets
-    
+    brackets: typing.List[Bracket]
+
     def __str__(self):
         return "(\n" + "\n".join(str(b) for b in self.brackets) + "\n)"
-    
-    def __repr__(self):
-        return "Schedule({})".format(self.brackets)
 
 
 def get_text(url: str, filename: str):
@@ -59,39 +53,43 @@ def find_schedule(soup: Tag, name: str):
 def parse_schedule(table: Tag):
     rate_regex = re.compile(r"(\d+(?:\.\d+)?)%")
     brackets = []
-    for row in table.tbody('tr'):
-        data = row('td')
-        lower = Decimal(data[0].string.strip().lstrip('$').replace(',', ''))
+    for row in table.tbody("tr"):
+        data = row("td")
+        lower = Decimal(data[0].string.strip().lstrip("$").replace(",", ""))
         try:
-            upper = Decimal(data[1].string.strip().lstrip('$').replace(',', ''))
+            upper = Decimal(data[1].string.strip().lstrip("$").replace(",", ""))
         except decimal.InvalidOperation:
-            upper = Decimal('Infinity')
+            upper = Decimal("Infinity")
         rate = Decimal(rate_regex.search(data[2].string).group(1))
         brackets.append(Bracket(lower, upper, rate))
     return Schedule(brackets)
 
 
-text = get_text(p17_url, p17_file)
-soup = BeautifulSoup(text, "lxml")
-book = soup.find(class_="book")
-# soup = BeautifulSoup(text, "html.parser")
+def main():
+    text = get_text(p17_url, p17_file)
+    soup = BeautifulSoup(text, "lxml")
+    book = soup.find(class_="book")
+    # soup = BeautifulSoup(text, "html.parser")
 
-tax_schedules = book.find(string=re.compile(r"^\d{4} Tax Rate Schedules$")).find_parent("div", class_="section")
+    tax_schedules = book.find(
+        string=re.compile(r"^\d{4} Tax Rate Schedules$")
+    ).find_parent("div", class_="section")
 
-tax_schedule_single = find_schedule(tax_schedules, "Schedule X")
-tax_schedule_married_joint = find_schedule(tax_schedules, "Schedule Y-1")
-tax_schedule_married_separate = find_schedule(tax_schedules, "Schedule Y-2")
-tax_schedule_head = find_schedule(tax_schedules, "Schedule Z")
+    tax_schedule_single = find_schedule(tax_schedules, "Schedule X")
+    tax_schedule_married_joint = find_schedule(tax_schedules, "Schedule Y-1")
+    tax_schedule_married_separate = find_schedule(tax_schedules, "Schedule Y-2")
+    tax_schedule_head = find_schedule(tax_schedules, "Schedule Z")
 
-single = parse_schedule(tax_schedule_single)
-married_joint = parse_schedule(tax_schedule_married_joint)
-married_separate = parse_schedule(tax_schedule_married_separate)
-head = parse_schedule(tax_schedule_head)
+    single = parse_schedule(tax_schedule_single)
+    married_joint = parse_schedule(tax_schedule_married_joint)
+    married_separate = parse_schedule(tax_schedule_married_separate)
+    head = parse_schedule(tax_schedule_head)
 
-print("Single:", single)
-print("Married joint:", married_joint)
-print("Married separate:", married_separate)
-print("Head of household:", head)
+    print("Single:", single)
+    print("Married joint:", married_joint)
+    print("Married separate:", married_separate)
+    print("Head of household:", head)
 
-# if __name__ == "__main__":
-#     main()
+
+if __name__ == "__main__":
+    main()
